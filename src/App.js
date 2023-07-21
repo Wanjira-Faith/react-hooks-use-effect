7f85412574f573eea21e09ccb4c05e22ab2b1a8c
+ <+>
2023-07-21 14:20:25 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(0);
+  const [text, setText]= useState("");
 
+  useEffect(() => {
+    console.log("useEffect called")
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    console.log("timeout");
+    setTimeout(() => setCount(0), 5000);
+    }, []);
+  
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+  <>
+    <button onClick={() => setCount((count) => count +1 )}>I've been clicked {count} times</button>
+    <input type="text" placeholder="Type away..." value={text} onChange={(e) => setText(e.target.value)} />
+  </>
+  );
 }
 
 export default App;
+
+    //useEffect Dependencies Cheatsheet
+//1.useEffect(() => {}): No dependencies array
+//     Run the side effect every time our component renders (whenever state or props change)
+// 2.useEffect(() => {}, []): Empty dependencies array
+//     Run the side effect only the first time our component renders
+//3.useEffect(() => {}, [variable1, variable2]): Dependencies array with elements in it
+//     Run the side effect any time the variable(s) change
